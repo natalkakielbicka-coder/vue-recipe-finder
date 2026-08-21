de0e808ca29e22cdab57517538636379ae2e2606
nkpiksalia <natalkakielbicka@gmail.com>
2026-08-21 21:18:16 +0200

    Add search states and loading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,16 @@ import RecipeCard from '@/components/RecipeCard.vue'
 
 const searchQuery = ref('')
 const recipes = ref([])
+const isLoading = ref(false)
 
 async function searchRecipes() {
+  if (!searchQuery.value.trim()) {
+    recipes.value = []
+    return
+  }
+
+  isLoading.value = true
+
   const response = await fetch(
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery.value}`,
   )
@@ -13,6 +21,8 @@ async function searchRecipes() {
   const data = await response.json()
 
   recipes.value = data.meals || []
+
+  isLoading.value = false
 }
 </script>
 
@@ -21,9 +31,15 @@ async function searchRecipes() {
     <h1>Recipe Finder</h1>
     <p>Znajdź pomysł na swój następny posiłek.</p>
 
-    <input v-model="searchQuery" type="text" placeholder="Wpisz nazwę dania..." />
+    <form @submit.prevent="searchRecipes">
+      <input v-model="searchQuery" type="text" placeholder="Wpisz nazwę dania..." />
 
-    <button @click="searchRecipes">Szukaj</button>
+      <button type="submit">Szukaj</button>
+    </form>
+
+    <p v-if="isLoading">Ładowanie...</p>
+
+    <p v-else-if="searchQuery && recipes.length === 0">Brak wyników.</p>
 
     <div class="recipes">
       <RecipeCard v-for="recipe in recipes" :key="recipe.idMeal" :recipe="recipe" />
