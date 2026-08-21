9e10140c4a98266c0ba7a049b3a9eaa44f432d9d
nkpiksalia <natalkakielbicka@gmail.com>
2026-08-21 21:10:31 +0200

    Add recipe search and recipe cards

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  recipe: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <article class="recipe-card">
+    <img :src="recipe.strMealThumb" :alt="recipe.strMeal" />
+
+    <div class="recipe-card__content">
+      <h2>{{ recipe.strMeal }}</h2>
+
+      <p>
+        {{ recipe.strCategory }}
+      </p>
+
+      <p>
+        {{ recipe.strArea }}
+      </p>
+    </div>
+  </article>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <script setup>
 import { ref } from 'vue'
+import RecipeCard from '@/components/RecipeCard.vue'
 
 const searchQuery = ref('')
+const recipes = ref([])
 
-function searchRecipes() {
-  console.log('Szukam:', searchQuery.value)
+async function searchRecipes() {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery.value}`,
+  )
+
+  const data = await response.json()
+
+  recipes.value = data.meals || []
 }
 </script>
 
@@ -16,6 +24,10 @@ function searchRecipes() {
     <input v-model="searchQuery" type="text" placeholder="Wpisz nazwę dania..." />
 
     <button @click="searchRecipes">Szukaj</button>
+
+    <div class="recipes">
+      <RecipeCard v-for="recipe in recipes" :key="recipe.idMeal" :recipe="recipe" />
+    </div>
 
     <p>Szukasz: {{ searchQuery }}</p>
   </section>
