752beb7d3f3905517f14fbde2df96055121e36eb
nkpiksalia <natalkakielbicka@gmail.com>
2026-08-21 20:53:41 +0200

    Clean Vue starter and add search input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const searchQuery = ref('')
+
+function searchRecipes() {
+  console.log('Szukam:', searchQuery.value)
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section class="home">
+    <h1>Recipe Finder</h1>
+    <p>Znajdź pomysł na swój następny posiłek.</p>
+
+    <input v-model="searchQuery" type="text" placeholder="Wpisz nazwę dania..." />
+
+    <button @click="searchRecipes">Szukaj</button>
+
+    <p>Szukasz: {{ searchQuery }}</p>
+  </section>
 </template>
